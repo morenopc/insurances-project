0c22b81b5b93effc01e6e56056f893bd48542f16
Moreno Cunha <moreno.pinheiro@gmail.com>
2018-02-10 10:35:48 -0200

    Serialize enum group and value

--- risks/serializers.py
+++ risks/serializers.py
@@ -1,20 +1,45 @@
 # -*- coding: UTF-8 -*-
 from collections import OrderedDict
 from rest_framework import serializers
-from eav.models import Value, Attribute
+from eav.models import Value, Attribute, EnumValue, EnumGroup
 from risks.models import Risk
+
+
+class EnumValueModelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = EnumValue
+        fields = ('value', )
+
+
+class EnumGroupModelSerializer(serializers.ModelSerializer):
+
+    enums = EnumValueModelSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = EnumGroup
+        fields = ('__all__')
 
 
 class AttributeModelSerializer(serializers.ModelSerializer):
 
+    enum_group = EnumGroupModelSerializer(read_only=True)
+
     class Meta:
         model = Attribute
-        fields = ('slug', 'datatype')
+        fields = ('name', 'slug', 'datatype', 'enum_group')
+
+    def to_representation(self, instance):
+        ret = super(AttributeModelSerializer, self).to_representation(instance)
+        # filter the null values and creates a new dictionary
+        ret = OrderedDict(list(filter(lambda x: x[1], ret.items())))
+        return ret
 
 
 class ValueModelSerializer(serializers.ModelSerializer):
 
     attribute = AttributeModelSerializer(read_only=True)
+    value_enum = EnumValueModelSerializer(read_only=True)
 
     class Meta:
         model = Value
